bbd77e39451d9ea41b96d8fbb96dadfbf835a22d
vmonegatto1 <62448677+vmonegatto1@users.noreply.github.com>
2020-11-18 02:18:56 -0300

    .

---
+++ Exercicios/ep4/ep4.c
@@ -0,0 +1,57 @@
+/*    
+ \__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__
+
+  AO PREENCHER ESSE CABEÇALHO COM O MEU NOME E O MEU NÚMERO USP, 
+  DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESSE PROGRAMA. 
+  TODAS AS PARTES ORIGINAIS DESSE EXERCÍCIO-PROGRAMA (EP) FORAM 
+  DESENVOLVIDAS E IMPLEMENTADAS POR MIM SEGUINDO AS INSTRUÇÕES DESSE EP
+  E QUE PORTANTO NÃO CONSTITUEM PLÁGIO. DECLARO TAMBÉM QUE SOU RESPONSÁVEL
+  POR TODAS AS CÓPIAS DESSE PROGRAMA E QUE EU NÃO DISTRIBUI OU FACILITEI A
+  SUA DISTRIBUIÇÃO. ESTOU CIENTE QUE OS CASOS DE PLÁGIO SÃO PUNIDOS COM 
+  REPROVAÇÃO DIRETA NA DISCIPLINA.
+
+  Nome: Lucas Ventura Monegatto
+  NUSP: 10731901
+
+  ep4.c
+
+  Referências: Com exceção das rotinas fornecidas no esqueleto e em sala
+  de aula, caso você tenha utilizado alguma referência, liste-as abaixo
+  para que o seu programa não seja considerada plágio.
+  Exemplo:
+
+  -
+ \__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__
+*/
+
+#include <stdio.h>
+
+/* ------------------------ */
+/* -------PROTÓTIPOS------- */
+/* ------------------------ */
+
+void limpaLinha();
+
+
+/* ------------------ */
+/* -------MAIN------- */
+/* ------------------ */
+
+int main()
+{
+
+    int;
+
+    return 0;
+}
+
+/* --------------------- */
+/* -------FUNÇÕES------- */
+/* --------------------- */
+
+void limpaLinha()
+{
+    char a = 0;
+    while (a != '\n')
+        scanf("%c", &a);
+}
